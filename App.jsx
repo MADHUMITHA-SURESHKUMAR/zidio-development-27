d2f78f6036c2d2ec28c0065fe56feaf7889ab0ef
MADHUMITHA S <160626427+MADHUMITHA-SURESHKUMAR@users.noreply.github.com>
2024-05-15 17:17:11 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import './App.css';
+function App() {
+  return (
+    <div className='text-white h-[100vh] flex items-center justify-center bg-cover' style={{"backgroundImage":"url('../src/assets/screenshot (611).png')"}}>
+      <Login/>
+    </div>
+  )
+}
+export default Appz
